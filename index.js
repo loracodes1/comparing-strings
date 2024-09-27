8275990841c7b1c450cb98446b5f5146cc81c3e6
florence <loracodes3@gmail.com>
2024-09-27 14:37:37 +0300

    comparison of numbers as strings

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ else {
 //output =>a is less than b
 //JavaScript treats strings as sequences of characters, and characters are compared based on their Unicode values. In this case, 'a' comes before 'b' in the Unicode table, so 'a' is considered "less than" 'b'.
 
+
+
+//when you put numbers instead of alphabets as strings
+//When comparing strings lexicographically:
+//The comparison starts from the first character of each string.
+//The Unicode value of '3' (the first character of "30") is 51.
+//The Unicode value of '4' (the first character of "40") is 52.
+//Since 51 (for '3') is less than 52 (for '4'), the condition num1 < num2 evaluates to true.
 const num1="30";
 const num2="40";
 if (num1<num2){
@@ -24,4 +32,5 @@ else {
     console.log(`${num1} and ${num2} are equal`)
 }
 
-//output=>
+//output=>30 is less than 40
+//EXPLANATION
