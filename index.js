ad13aa073aed10d8bf8a3d4fde6f5e158bea2e58
florence <loracodes3@gmail.com>
2024-09-27 14:34:42 +0300

    compare a and b string

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const a=30;
-const b=10;
-if (a>b){
-    console.log()
+const a='a';
+const b='b';
+if (a<b){
+    console.log(`${a} is less than ${b}`)
 }
+else if (a>b){
+    console.log(`${a} is less than ${b}`) 
+}
+else {
+    console.log(`${a} and ${b} are equal`)
+}
+//output =>a is less than b
+//JavaScript treats strings as sequences of characters, and characters are compared based on their Unicode values. In this case, 'a' comes before 'b' in the Unicode table, so 'a' is considered "less than" 'b'.
+
+const num1="30";
+const num2="40";
+if (num1<num2){
+    console.log(`${num1} is less than ${num2}`)
+}
+else if (a>b){
+    console.log(`${num1} is less than ${num2}`) 
+}
+else {
+    console.log(`${num1} and ${num2} are equal`)
+}
+
+//output=>
